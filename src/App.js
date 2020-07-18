5bec4d604a356f29e1d3d3b19d7685aec2aca666
Kevin Peter <29401940+KevzPeter@users.noreply.github.com>
2020-07-18 05:49:08 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -70,7 +70,7 @@ class App extends Component{
     
     if(city && country){
       
-    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${api_key}`);
+    const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${api_key}`);
     const response = await api_call.json();
     this.setState(
       {
